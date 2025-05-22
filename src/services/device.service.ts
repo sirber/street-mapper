b1c7742ed7941f1544b5c96dda7e96bc84f8257c
Stéphane Bérubé <sirber83@gmail.com>
2025-05-21 21:01:55 -0400

    added: deviceByID() added: deviceByUUID()

--- src/services/device.service.ts
+++ src/services/device.service.ts
@@ -1,8 +1,14 @@
 import db from '../libraries/database';
 import type { Device } from '../types/device.type';
 
-export async function deviceExists(uuid: string): Promise<boolean> {
-	const result = await db<Device>('device').where('external_id', uuid).first();
+export async function deviceByID(id: number): Promise<Device | undefined> {
+	return await db<Device>('device').where('id', id).first();
+}
 
-	return !!result;
+export async function deviceByUUID(uuid: string): Promise<Device | undefined> {
+	return await db<Device>('device').where('external_id', uuid).first();
+}
+
+export async function deviceExists(uuid: string): Promise<boolean> {
+	return !!(await deviceByUUID(uuid));
 }
